ca0427083e9f1adaddaa3e83fb250902199b0c12
Xessaki <ouma.ivanov@yandex.ru>
2025-02-22 17:01:08 +0300

    Правки в дз

--- DZ_1_2.py
+++ DZ_1_2.py
@@ -1,22 +1,26 @@
 # Дано число. Выведите в консоль первую цифру этого числа.
 
-number = input('Введите число\n')
+number = 103
+number = str(number)
 print(number[0])
 
 # Дано число. Выведите в консоль последнюю цифру этого числа.
 
-number = input('Введите число\n')
+number = 103
+number = str(number)
 print(number[-1])
 
 # Дано число. Выведите в консоль сумму первой и последней цифры этого числа.
 
-number = input('Введите число\n')
+number = 103
+number = str(number)
 Sum = int(number[0]) + int(number[-1])
 print(Sum)
 
 # Дано число. Выведите количество цифр в этом числе.
 
-number = input('Введите число\n')
+number = 103
+number = str(number)
 print(len(number))
 
 # Дан список: [1, 2, 3, 4, 5, 6] получить из него срез: [1, 2, 3]
